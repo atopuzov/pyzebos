0bb75b28c819731ebb5dfa5a48d22d0f93d1eebb
Aleksandar Topuzovic <aleksandar.topuzovic@gmail.com>
2015-02-27 16:51:04 +0300

    Ip route tests

--- tests/test_ip_route.py
+++ tests/test_ip_route.py
@@ -4,3 +4,46 @@ import pytest
 
 
 ipRouteParser = ipRoute + stringEnd
+
+route_statements = [
+    'ip route 1.2.3.0 255.255.255.0 8.8.8.8 200 description Route_to_somewhere',
+    'ip route 1.2.3.0 255.255.255.0 8.8.8.8 200 tag 20 description Route_to_somewhere',
+    'ip route 1.2.3.0 255.255.255.0 8.8.8.8 200 tag 20',
+    'ip route 1.2.3.0 255.255.255.0 8.8.8.8 200',
+    'ip route 1.2.3.0 255.255.255.0 8.8.8.8 description Route_to_somewhere',
+    'ip route 1.2.3.0 255.255.255.0 8.8.8.8 tag 20 description Route_to_somewhere',
+    'ip route 1.2.3.0 255.255.255.0 8.8.8.8 tag 20',
+    'ip route 1.2.3.0 255.255.255.0 8.8.8.8',
+    'ip route 1.2.3.0 255.255.255.0 vlan0 200 description Route_to_somewhere',
+    'ip route 1.2.3.0 255.255.255.0 vlan0 200 tag 20 description Route_to_somewhere',
+    'ip route 1.2.3.0 255.255.255.0 vlan0 200 tag 20',
+    'ip route 1.2.3.0 255.255.255.0 vlan0 200',
+    'ip route 1.2.3.0 255.255.255.0 vlan0 description Route_to_somewhere',
+    'ip route 1.2.3.0 255.255.255.0 vlan0 tag 20 description Route_to_somewhere',
+    'ip route 1.2.3.0 255.255.255.0 vlan0 tag 20',
+    'ip route 1.2.3.0 255.255.255.0 vlan0',
+    'ip route 1.2.3.0/24 8.8.8.8 200 description Route_to_somewhere',
+    'ip route 1.2.3.0/24 8.8.8.8 200 tag 20 description Route_to_somewhere',
+    'ip route 1.2.3.0/24 8.8.8.8 200 tag 20',
+    'ip route 1.2.3.0/24 8.8.8.8 200',
+    'ip route 1.2.3.0/24 8.8.8.8 description Route_to_somewhere',
+    'ip route 1.2.3.0/24 8.8.8.8 tag 20 description Route_to_somewhere',
+    'ip route 1.2.3.0/24 8.8.8.8 tag 20',
+    'ip route 1.2.3.0/24 8.8.8.8',
+    'ip route 1.2.3.0/24 vlan0 200 description Route_to_somewhere',
+    'ip route 1.2.3.0/24 vlan0 200 tag 20 description Route_to_somewhere',
+    'ip route 1.2.3.0/24 vlan0 200 tag 20',
+    'ip route 1.2.3.0/24 vlan0 200',
+    'ip route 1.2.3.0/24 vlan0 description Route_to_somewhere',
+    'ip route 1.2.3.0/24 vlan0 tag 20 description Route_to_somewhere',
+    'ip route 1.2.3.0/24 vlan0 tag 20',
+    'ip route 1.2.3.0/24 vlan0',
+]
+
+def test_ip_route_parse_ok():
+    for route_statement in route_statements:
+        try:
+            tokens = ipRouteParser.parseString(route_statement)
+        except ParseException:
+            print "ip route: {}".format(route_statement)
+            raise
